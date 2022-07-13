021d63eeab40207588cc26353d77801016f2d95b
mrkuh <michi.wajand@gmail.com>
2022-07-13 12:42:21 +0200

    New User

--- wwwroot/js/search.js
+++ wwwroot/js/search.js
@@ -1,4 +1,20 @@
-﻿function filterlist() {
+﻿function filterPrjectlist() {
+    var input, filter, li, t, i, txtValue;
+    input = document.getElementById("keyWordInput");
+    filter = input.value.toUpperCase();
+    li = document.getElementsByClassName("projectItem");
+    for (i = 0; i < li.length; i++) {
+        t = li[i].getElementsByTagName("h4")[0];
+        txtValue = t.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "flex";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function filterTaskslist() {
     var input, filter, li, t, i, txtValue;
     input = document.getElementById("keyWordInput");
     filter = input.value.toUpperCase();
